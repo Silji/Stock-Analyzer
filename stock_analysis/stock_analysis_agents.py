df60b962fad95c6048697d289bd392b677e067ea
Silji <silji.abraham@gmail.com>
2024-04-24 16:54:20 -0400

    changed

--- stock_analysis/stock_analysis_agents.py
+++ stock_analysis/stock_analysis_agents.py
@@ -10,7 +10,7 @@ from langchain.tools.yahoo_finance_news import YahooFinanceNewsTool
 class StockAnalysisAgents():
   def financial_analyst(self):
     return Agent(
-      role='The Best Financial Analyst',
+      role='An Expert Financial Analyst',
       goal="""Impress all customers with your financial data 
       and market trends analysis""",
       backstory="""The most seasoned financial analyst with 
